7bc51b8bd3c5eadb8b6becd0d95ec933d2248945
KRPpang <jinjonathan123@gmail.com>
2025-02-16 14:50:19 -0800

    Update ApiController.java

--- Backend/src/main/java/com/CS4800_DJ3/StudyDeckBackend/Controller/ApiController.java
+++ Backend/src/main/java/com/CS4800_DJ3/StudyDeckBackend/Controller/ApiController.java
@@ -15,9 +15,6 @@ import com.CS4800_DJ3.StudyDeckBackend.Models.*;
 import com.CS4800_DJ3.StudyDeckBackend.Repo.*;
 import com.CS4800_DJ3.StudyDeckBackend.Service.*;
 
-
-
-
 @RestController
 @RequestMapping("/api")
 public class ApiController {
@@ -31,7 +28,7 @@ public class ApiController {
         this.deckProgressService = deckProgressService;
     }
 
-    // 
+    //
 
     @Autowired
     private AccountRepo accountRepo;
@@ -42,11 +39,11 @@ public class ApiController {
     @Autowired
     private StudyDeckRepo studyDeckRepo;
 
-
     /**
      * API endpoint to login
+     * 
      * @param AccountRequest: request body containing username and password
-     * @param session: session object to store user information
+     * @param session:        session object to store user information
      * @return: response entity containing success or error message
      */
     @PostMapping(value = "/login")
@@ -60,7 +57,7 @@ public class ApiController {
             return ResponseEntity.ok(Map.of("error", "User does not exist"));
         }
 
-        if (!account.checkPassword(password)){
+        if (!account.checkPassword(password)) {
             return ResponseEntity.ok(Map.of("error", "Incorrect password"));
         }
 
@@ -70,9 +67,9 @@ public class ApiController {
         return ResponseEntity.ok(Map.of("success", "Logged in"));
     }
 
-
     /**
      * Create a new account
+     * 
      * @param AccountRequest: request body containing username and password
      * @return response entity containing success or error message
      */
@@ -84,7 +81,7 @@ public class ApiController {
         if (accountRepo.findByUsername(username) != null) {
             return ResponseEntity.ok(Map.of("error", "Username already exists"));
         }
-        
+
         Account account = new Account();
         account.setUsername(username);
         account.setPassword(password);
@@ -94,9 +91,9 @@ public class ApiController {
         return ResponseEntity.ok(Map.of("success", "Account created"));
     }
 
-
     /**
      * API endpoint to retrieve all decks owned by the user
+     * 
      * @param session: session object to store user information
      * @return: response entity containing list of study decks
      */
@@ -105,7 +102,8 @@ public class ApiController {
 
         Long userID = (Long) session.getAttribute("userID");
 
-        if(userID == null) return ResponseEntity.ok(Map.of("error", "User not logged in"));
+        if (userID == null)
+            return ResponseEntity.ok(Map.of("error", "User not logged in"));
 
         System.out.println("TEST");
 
@@ -114,9 +112,9 @@ public class ApiController {
         return ResponseEntity.ok(Map.of("studyDeckList", studyDeckList));
     }
 
-
     /**
      * API endpoint to retrieve a specific deck
+     * 
      * @param deckSelection: request body containing deckID
      * @return: response entity containing the deck
      */
@@ -128,61 +126,59 @@ public class ApiController {
         boolean isOwner = false;
 
         // Check if deck exists
-        if(studyDeck == null) {
+        if (studyDeck == null) {
             return ResponseEntity.ok(Map.of("error", "Deck does not exist"));
         }
 
         // Check if deck is public or not, if not public, other users cannot access it
         if (!studyDeck.isPublic()) {
-            if(currUserID == null) {
+            if (currUserID == null) {
                 return ResponseEntity.ok(
-                    Map.of(
-                        "error",
-                        "Deck is set to private, if you are the owner, please login to access it"
-                    ));
+                        Map.of(
+                                "error",
+                                "Deck is set to private, if you are the owner, please login to access it"));
             }
-            if(!(studyDeck.getOwnerID() == currUserID)) {
-                return ResponseEntity.ok(Map.of("error", "Deck is set to private" ));
+            if (!(studyDeck.getOwnerID() == currUserID)) {
+                return ResponseEntity.ok(Map.of("error", "Deck is set to private"));
             }
         }
 
-        // Check if user has any progress on the deck, if not, create a new progress entry for the user in the deck_progress table
-        if(currUserID != null){
+        // Check if user has any progress on the deck, if not, create a new progress
+        // entry for the user in the deck_progress table
+        if (currUserID != null) {
             isOwner = studyDeck.getOwnerID() == ((Long) session.getAttribute("userID"));
-            DeckProgress deckProgress = 
-                deckProgressRepo.findByUserIDAndDeckID(currUserID, deckSelection.getDeckID());
-            
-            if(deckProgress == null) {
-                DeckProgress newDeckProgress = 
-                    deckProgressService.copyStudyDeckToProgress(
+            DeckProgress deckProgress = deckProgressRepo.findByUserIDAndDeckID(currUserID, deckSelection.getDeckID());
+
+            if (deckProgress == null) {
+                DeckProgress newDeckProgress = deckProgressService.copyStudyDeckToProgress(
                         deckSelection.getDeckID(),
                         currUserID,
-                        studyDeck
-                    );
+                        studyDeck);
                 return ResponseEntity.ok(
-                    Map.of("deck", newDeckProgress, "owner", isOwner, "trackProgress", true));
-            }
-            else{
+                        Map.of("deck", newDeckProgress, "owner", isOwner, "trackProgress", true));
+            } else {
                 return ResponseEntity.ok(
-                    Map.of("deck", deckProgress, "owner", isOwner, "trackProgress", true));
+                        Map.of("deck", deckProgress, "owner", isOwner, "trackProgress", true));
             }
         }
 
         return ResponseEntity.ok(Map.of("deck", studyDeck, "owner", false, "trackProgress", false));
     }
 
-
     /**
      * API endpoint to create a new deck
+     * 
      * @param StudyDeckCreateRequest: request body containing deck information
-     * @param session: session object to store user information
+     * @param session:                session object to store user information
      * @return response entity containing success or error message
      */
     @PostMapping(value = "/createDeck")
-    public ResponseEntity<?> createDeck(@RequestBody StudyDeckCreateRequest studyDeckCreateRequest, HttpSession session) {
+    public ResponseEntity<?> createDeck(@RequestBody StudyDeckCreateRequest studyDeckCreateRequest,
+            HttpSession session) {
         Long userID = (Long) session.getAttribute("userID");
 
-        if(userID == null) return ResponseEntity.ok(Map.of("error", "User not logged in")) ;
+        if (userID == null)
+            return ResponseEntity.ok(Map.of("error", "User not logged in"));
 
         StudyDeck studyDeck = new StudyDeck();
         studyDeck.setOwnerID(userID);
@@ -191,7 +187,7 @@ public class ApiController {
 
         List<FlashCard> content = new ArrayList<>();
 
-        for(FlashCard request : studyDeckCreateRequest.getContent()) {
+        for (FlashCard request : studyDeckCreateRequest.getContent()) {
             FlashCard newCard = new FlashCard();
             newCard.setQuestion(request.getQuestion());
             newCard.setAnswer(request.getAnswer());
@@ -204,11 +200,11 @@ public class ApiController {
         return ResponseEntity.ok(Map.of("success", "Deck created"));
     }
 
-
     /**
      * API endpoint to update a deck
+     * 
      * @param StudyDeckEditRequest: request body containing deck information
-     * @param session: session object to store user information
+     * @param session:              session object to store user information
      * @return response entity containing success or error message
      */
     @Transactional
@@ -216,35 +212,35 @@ public class ApiController {
     public ResponseEntity<?> updateDeck(@RequestBody StudyDeckEditRequest studyDeckEditRequest, HttpSession session) {
         Long userID = (Long) session.getAttribute("userID");
 
-        if(userID == null) return ResponseEntity.ok(
-            Map.of("error", "User not logged in")) ;
+        if (userID == null)
+            return ResponseEntity.ok(
+                    Map.of("error", "User not logged in"));
 
         StudyDeck studyDeck = studyDeckRepo.findByDeckID(studyDeckEditRequest.getDeckID());
 
-        if(studyDeck == null) return ResponseEntity.ok(
-            Map.of("error", "Deck does not exist"));
+        if (studyDeck == null)
+            return ResponseEntity.ok(
+                    Map.of("error", "Deck does not exist"));
 
-        if(studyDeck.getOwnerID() != userID) return ResponseEntity.ok(
-            Map.of("error", "You do not own this deck"));
-        
+        if (studyDeck.getOwnerID() != userID)
+            return ResponseEntity.ok(
+                    Map.of("error", "You do not own this deck"));
 
-        if(studyDeck.isPublic() != studyDeckEditRequest.isPublic()) {
+        if (studyDeck.isPublic() != studyDeckEditRequest.isPublic()) {
             studyDeckRepo.updatePublic(
-                studyDeck.getDeckID(),
-                studyDeckEditRequest.isPublic()
-            );
+                    studyDeck.getDeckID(),
+                    studyDeckEditRequest.isPublic());
         }
 
-        if(!studyDeck.getDeckName().equals(studyDeckEditRequest.getDeckName())) {
+        if (!studyDeck.getDeckName().equals(studyDeckEditRequest.getDeckName())) {
             studyDeckRepo.updateName(
-                studyDeck.getDeckID(),
-                studyDeckEditRequest.getDeckName()
-            );
+                    studyDeck.getDeckID(),
+                    studyDeckEditRequest.getDeckName());
         }
 
         List<FlashCard> content = new ArrayList<>();
 
-        for(FlashCard request : studyDeckEditRequest.getContent()) {
+        for (FlashCard request : studyDeckEditRequest.getContent()) {
             FlashCard newCard = new FlashCard();
             newCard.setQuestion(request.getQuestion());
             newCard.setAnswer(request.getAnswer());
@@ -259,9 +255,9 @@ public class ApiController {
         return ResponseEntity.ok(Map.of("success", "Deck updated"));
     }
 
-    
     /**
      * API endpoint to logout
+     * 
      * @param session: session object to store user information
      * @return: response entity containing success or error message
      */
@@ -270,4 +266,10 @@ public class ApiController {
         session.invalidate();
         return ResponseEntity.ok(Map.of("success", "Logged out"));
     }
+
+    @PostMapping(value = "/logout")
+    public ResponseEntity<?> checkin(HttpSession session) {
+        return ResponseEntity.ok(Map.of("success", "Hi there, my name is jonathan"));
+    }
+
 }
