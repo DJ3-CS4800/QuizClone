b33860f5eb39eb2302e03d21b05562b09a042454
jawsefPedroza <josephpedroza77@gmail.com>
2025-03-03 22:03:53 -0800

    small update on register

--- Frontend/src/pages/Register_page.jsx
+++ Frontend/src/pages/Register_page.jsx
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/navbar.css";
-import "../styles/login.css";
-import "../styles/footer.css";
-import "../styles/sections.css";
-import "../styles/buttons.css";
 import "../styles/register_page.css";
 
 const RegisterPage = () => {
